5bde00991baf503b9b9ccd1273b6cba7fa611c79
holy_road_ss <lolilysuzume893@gmail.com>
2021-06-15 10:28:49 +0000

    add size

--- pkg/proc/export.go
+++ pkg/proc/export.go
@@ -40,6 +40,9 @@ func ExportToml(tomlPath, env, settingTomlPath string) (err error) {
 		columnLines := []string{}
 		for _, col := range ti.columns {
 			columnLine := fmt.Sprintf(`    {name = "%v", type = "%v"`, col.name, col.columnType)
+			if shouldAddColumnSize(col.columnType, col.size, col.unsigned) {
+				columnLine += fmt.Sprintf(`, size = "%v"`, col.size)
+			}
 			if col.unsigned {
 				columnLine += fmt.Sprintf(`, unsigned = true`)
 			}
@@ -70,6 +73,43 @@ func ExportToml(tomlPath, env, settingTomlPath string) (err error) {
 		result[len(result)-1] += "\n"
 	}
 	fmt.Println(strings.Join(result, "\n"))
+
+	return
+}
+
+func shouldAddColumnSize(columnType, size string, unsigned bool) (result bool) {
+	switch columnType {
+	case "varchar":
+		result = true
+	case "int":
+		if unsigned && size != "10" {
+			result = true
+		} else if !unsigned && size != "11" {
+			result = true
+		}
+	case "bigint":
+		if size != "20" {
+			result = true
+		}
+	case "tinyint":
+		if unsigned && size != "3" {
+			result = true
+		} else if !unsigned && size != "4" {
+			result = true
+		}
+	case "smallint":
+		if unsigned && size != "5" {
+			result = true
+		} else if !unsigned && size != "6" {
+			result = true
+		}
+	case "mediumint":
+		if unsigned && size != "8" {
+			result = true
+		} else if !unsigned && size != "9" {
+			result = true
+		}
+	}
 
 	return
 }
